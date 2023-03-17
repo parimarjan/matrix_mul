45f7bbe13c4182717dd83a09241afd7d8d01a241
parimarjan <parimarjannegi@gmail.com>
2023-03-16 23:11:22 -0400

    minor

--- test.c
+++ test.c
@@ -7,7 +7,7 @@
 
 /* TODO: Support non-square matrices */
 void print_matrix(float **m, int rows, int cols) {
-     int r,c;	  
+     int r,c;
      printf("[ ");
      for (r = 0; r < rows; r++) {
 	  printf("[ ");
@@ -19,7 +19,7 @@ void print_matrix(float **m, int rows, int cols) {
 	  }
 	  printf(" ]\n");
      }
-     printf("]\n"); 
+     printf("]\n");
 }
 
 long compute_sum(struct matrix *mat) {
@@ -40,17 +40,17 @@ long compute_sum(struct matrix *mat) {
     return sum;
 }
 
-/* Runs an arbitrary multiply implementation and times it. 
+/* Runs an arbitrary multiply implementation and times it.
  * @f: matrix multiplication function.
  * @n: size of matrix (assuming square for simplicity)
  * @block: block size
  */
-void run_multiply(void (*f) (struct matrix *, struct matrix *, struct matrix*), int n, char *name, enum alloc_type type) 
+void run_multiply(void (*f) (struct matrix *, struct matrix *, struct matrix*), int n, char *name, enum alloc_type type)
 {
      struct matrix *a = rand_matrix(n,n,type);
      struct matrix *b = rand_matrix(n,n,type);
      struct matrix *c = alloc_matrix(n,n,type);
-    
+
      /* Time only the multiply function call */
      struct timeval start, end, diff;
      gettimeofday(&start, 0);
@@ -70,7 +70,7 @@ void run_multiply(void (*f) (struct matrix *, struct matrix *, struct matrix*), 
 }
 
 int main(int argc, char *argv[]) {
-    
+
     /* Technically, need to support non-square matrices, but all the results we care about are only
      * square matrices, so just accept n as a parameter */
     int n = 128;
@@ -87,28 +87,28 @@ int main(int argc, char *argv[]) {
                 exit(1);
         }
     }
-    /* Need a smarter way to choose block size */ 
+    /* Need a smarter way to choose block size */
     printf("n = %d\n", n);
     /* run it with different optimization schemes */
     run_multiply(mult_naive, n, "C-Naive", naive);
-    /*run_multiply(mult_naive_kj, n, "C-Naive-kj", naive);*/
-    /*run_multiply(mult_parallel_kj, n, "C-Parallel-kj", naive);*/
+    run_multiply(mult_naive_kj, n, "C-Naive-kj", naive);
+    run_multiply(mult_parallel_kj, n, "C-Parallel-kj", naive);
 
     /* attractive chaos based implementation */
-    /*run_multiply(ac_mat_mul0, n, "ac_naive", ac);*/
-    /*run_multiply(ac_mat_mul1, n, "ac_transposed", ac);*/
-    /*run_multiply(ac_mat_mul1_kj, n, "ac_transposed_kj", ac);*/
+    run_multiply(ac_mat_mul0, n, "ac_naive", ac);
+    run_multiply(ac_mat_mul1, n, "ac_transposed", ac);
+    run_multiply(ac_mat_mul1_kj, n, "ac_transposed_kj", ac);
 
 #ifdef __SSE__
-    /*run_multiply(ac_mat_mul2, n, "ac_sse", ac);*/
-    /*run_multiply(ac_mat_mul7, n, "ac_sse_blocked", ac);*/
+    run_multiply(ac_mat_mul2, n, "ac_sse", ac);
+    run_multiply(ac_mat_mul7, n, "ac_sse_blocked", ac);
 #endif
 
 /*#ifdef HAVE_CBLAS*/
     /*run_multiply(ac_mat_mul6, n, "ac_blas", ac);*/
 /*#endif*/
-    
+
     /* contiguous arrays */
-    /*run_multiply(contig_naive_kj, n, "contig_naive", contig);*/
-    /*run_multiply(contig_blocked, n, "contig_blocked", contig);*/
+    run_multiply(contig_naive_kj, n, "contig_naive", contig);
+    run_multiply(contig_blocked, n, "contig_blocked", contig);
 }
